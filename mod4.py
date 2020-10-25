56aa9ba9beef316384fa8ec1d2183e6a29f0feca
mai933 <71470255+mai933@users.noreply.github.com>
2020-10-25 10:37:19 -0400

    Add files via upload

---
+++ mod4.py
@@ -0,0 +1,30 @@
+"""
+
+This is a script that prompts the user to enter email addresses which adds them to
+a list and prints the list.
+
+"""
+
+
+addresses = []
+
+more = input("Do you have an email address to enter (y/n)? ") 
+
+
+while more == "y": 
+    email = input("Enter the address: ") 
+    addresses.append(email)
+    break
+        #url to add new user to the rooms
+        add_membership_url = 'https://web.webex.com/spaces/aHR0cHM6Ly9jb252LWEud2J4Mi5jb20vY29udmVyc2F0aW9uL2FwaS92MS9jb252ZXJzYXRpb25zLzRiMGUzZDIwLTBkYjgtMTFlYi04ZjhmLTFkZjJhMWU4YmY4ZQ=='
+           
+        print 
+        for email in emails: # every email address in the lines list 
+        try:
+        param = (
+        "roomId": SDN Postman Room,
+          "personalEmail": mai933@hotmail.com,
+          )
+    
+print(addresses)  
+
